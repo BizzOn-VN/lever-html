17f2c5df9bbe33735de8cddd57fce94d50935158
chikien111 <38467294+chikien111@users.noreply.github.com>
2022-12-29 12:46:16 +0700

    update new-design

--- scripts/main.js
+++ scripts/main.js
@@ -90,7 +90,9 @@ jQuery(document).ready(function ($) {
             $('.ui.checkbox').checkbox();
         }
         var fancybox = function(){
+
             // $("#modal-sucess").fancybox().trigger('click');
+            $(".the-le").fancybox({});
         }
 
         var nav_tabs = function(){
@@ -137,3 +139,22 @@ jQuery(document).ready(function ($) {
    
 });    
     
+
+    document.querySelector("html").classList.add('js');
+
+var fileInput  = document.querySelector( ".input-file" ),  
+    button     = document.querySelector( ".input-file-trigger" ),
+    the_return = document.querySelector(".file-return");
+      
+button.addEventListener( "keydown", function( event ) {  
+    if ( event.keyCode == 13 || event.keyCode == 32 ) {  
+        fileInput.focus();  
+    }  
+});
+button.addEventListener( "click", function( event ) {
+   fileInput.focus();
+   return false;
+});  
+fileInput.addEventListener( "change", function( event ) {  
+    the_return.innerHTML = this.value;  
+});  
